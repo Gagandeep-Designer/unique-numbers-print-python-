73f26149bf930175ed50ecde2a3c37b3a7477b76
Gagan <166978135+Gagandeep-Designer@users.noreply.github.com>
2024-04-22 22:18:27 +0530

    Create unique_number.py

---
+++ unique_number.py
@@ -0,0 +1,12 @@
+# 2.write a program to input eight numbers from user and display all unique number
+number1=int(input("Enter 1st Number by user:-\n"))
+number2=int(input("Enter 2nd Number by user:-\n"))
+number3=int(input("Enter 3rd Number by user:-\n"))
+number4=int(input("Enter 4th Number by user:-\n"))
+number5=int(input("Enter 5th Number by user:-\n"))
+number6=int(input("Enter 6th Number by user:-\n"))
+number7=int(input("Enter 7th Number by user:-\n"))
+number8=int(input("Enter 8th Number by user:-\n"))
+
+unique_number={number1,number2,number3,number4,number5,number6,number7,number8}
+print(unique_number)
